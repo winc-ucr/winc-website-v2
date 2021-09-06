5954fb80af7ba9406f6ef4e3b63a05b90bbdb7bb
Andrei Dimaano <andreiidimaano@gmail.com>
2021-09-06 16:12:16 -0700

    setup website, add navbar, add hero image

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import { Link, navigate, graphql, useStaticQuery } from "gatsby";
+import "./navbar.css";
+
+const navLinks = [
+  { to: "/about", text: "About" },
+  { to: "/programs", text: "Programs" },
+  { to: "/events", text: "Events" },
+];
+
+const NavigationBar = () => {
+  return (
+    <div
+      className="navbar"
+      style={{
+        position: "fixed",
+        zIndex: 2,
+        top: 0,
+        padding: "0 2rem",
+        width: "100%",
+        height: "5.25rem",
+        display: "flex",
+        alignContent: "center",
+        backgroundColor: "#FA6C5E",
+        color: "white",
+      }}
+    >
+      <div
+        className="navbarContents"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <Link
+              style={{
+                display: "flex",
+                alignItems: "center",
+                color: "white",
+                textDecoration: "none",
+                fontFamily: "Manrope, sans-serif",
+                fontSize: "2rem",
+                fontWeight: "bold",
+              }}
+              to={"/"}
+            >
+              WINC
+            </Link>
+          </div>
+        </div>
+        <div className="navbarLinks" style={{ display: "flex" }}>
+          {navLinks.map((link) => (
+            <div className="navbarLink">
+              <Link
+                style={{ fontSize: "1.25rem", fontWeight: "bold" }}
+                to={link.to}
+              >
+                {link.text.toUpperCase()}
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div
+          className="navbarButton"
+          style={{
+            textAlign: "center",
+            fontSize: "1rem",
+            width: "10rem",
+            backgroundColor: "white",
+            color: "black",
+            border: "0.125rem solid black",
+          }}
+        >
+          JOIN NOW
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,70 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import Typist from "react-typist";
+import "../components/sections/home/home.css";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <div
+      style={{
+        backgroundColor: "#FA6C5E",
+      }}
+    >
+      <div
+        style={{
+          minHeight: "calc(100vh - 5.25rem)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <div>
+          <h1
+            style={{
+              fontFamily: "'Manrope', sans-serif",
+              color: "white",
+              fontSize: "2rem",
+            }}
+          >
+            We are
+          </h1>
+          <Typist className="heroTypist" avgTypingDelay={80}>
+            <span>Women in computing;</span>
+            <Typist.Backspace count={19} delay={100} />
+            <span>software engineers;</span>
+            <Typist.Backspace count={19} delay={100} />
+            <span>data scientists;</span>
+            <Typist.Backspace count={16} delay={100} />
+            <span>Women in computing;</span>
+          </Typist>
+        </div>
+        <div style={{ height: "auto", fontSize: "20rem" }}>👩‍💻</div>
+      </div>
+    </div>
+    <div
+      style={{
+        backgroundColor: "#FFEFEF",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "10rem",
+      }}
+    >
+      SPONSORS
+    </div>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
